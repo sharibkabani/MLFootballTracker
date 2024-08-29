3a6bbb889011131adeeb5430ff4f3ab6491603e4
Sharib Kabani <ksharib05@gmail.com>
2024-08-29 03:44:02 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+from utils import read_video, save_video
+from trackers import Tracker
+from team_assigner import TeamAssigner
+from player_ball_assigner import PlayerBallAssigner
+import numpy as np
+import cv2
+
+def main():
+    # Read video
+    video_frames = read_video("input_videos/test.mp4")
+    
+    # Create tracker 
+    tracker = Tracker("models/best.pt")
+    tracks = tracker.get_object_tracks(video_frames, read_from_stub=True)
+    
+
+    # Interpolate ball positions
+    tracks["ball"] = tracker.interpolate_ball_position(tracks["ball"])
+
+    # Assign player teams
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], tracks["players"][0])
+    
+    for frame_num, player_track in enumerate(tracks["players"]):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num], track["bbox"], player_id)
+            tracks["players"][frame_num][player_id]["team"] = team
+            tracks["players"][frame_num][player_id]["team_color"] = team_assigner.team_colors[team]
+    
+    # Get team colors
+    team_1_color = team_assigner.team_colors[1]
+    team_2_color = team_assigner.team_colors[2]
+    
+    # Assign ball to player
+    player_assigner = PlayerBallAssigner()
+    team_ball_control = []
+    for frame_num, player_track in enumerate(tracks["players"]):
+        ball_bbox = tracks["ball"][frame_num][1]["bbox"]
+        assigned_player = player_assigner.assign_ball_to_player(player_track, ball_bbox)
+        
+        if assigned_player != -1:
+            tracks["players"][frame_num][assigned_player]["has_ball"] = True
+            team_ball_control.append(tracks["players"][frame_num][assigned_player]["team"])
+        else:
+            team_ball_control.append(team_ball_control[-1])
+    team_ball_control = np.array(team_ball_control)
+    
+    # Draw bounding boxes
+    output_video_frames = tracker.draw_annotations(video_frames, tracks, team_ball_control, team_1_color, team_2_color)
+    
+    # Save video
+    save_video(output_video_frames, "output_videos/output_video.avi")
+    
+if __name__ == "__main__":
+    main()
